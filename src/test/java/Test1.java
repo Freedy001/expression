b984a29348c5e96d604390e81a94dcad4c9df5fe
Freedy001 <985948228@qq.com>
2022-08-29 22:12:03 +0800

    增加了远程代码提示功能

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 /**
  * @author Freedy
  * @date 2022/7/15 14:57
  */
 public class Test1 {
     public static void main(String[] args) {
-
-
+        System.out.println(Arrays.toString(Pattern.compile("aa,bb,cc").split(",")));
     }
 }
