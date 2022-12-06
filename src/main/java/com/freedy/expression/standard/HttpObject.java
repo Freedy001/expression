f5c975a81229b467885453f7106738e59c2467d3
Freedy！ <985948228@qq.com>
2022-12-06 10:56:27 +0800

    Delete src/main/java/com/freedy/expression directory

--- src/main/java/com/freedy/expression/standard/HttpObject.java
+++
@@ -1,8 +0,0 @@
-package com.freedy.expression.standard;
-
-/**
- * @author Freedy
- * @date 2022/6/30 1:29
- */
-public sealed interface HttpObject permits HttpResult, HttpReqParam {
-}
--- src/main/java/com/freedy/expression/standard/StringClassLoader.java
+++
@@ -1,9 +0,0 @@
-package com.freedy.expression.standard;
-
-/**
- * @author Freedy
- * @date 2022/1/10 14:40
- */
-public class StringClassLoader {
-    void test(){}
-}
